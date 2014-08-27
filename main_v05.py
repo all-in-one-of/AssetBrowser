945b49430b083522e1f75096534a29f7aaa10fb8
Schizo <yafes7@yahoo.de>
2014-08-27 20:05:34 +0200

    First Working Version
    Attraction Points not deleted

--- main_v05.py
+++ main_v05.py
@@ -1,3 +1,5 @@
+import copy
+
 node = hou.pwd()
 geo = node.geometry()
 nodeMultiplier = 1
@@ -26,24 +28,24 @@ class treeNode(object):
 			return -1
 
 		else:
-			#print "num of Influencors" + str(self.numOfInfluencors)
+			##print "num of Influencors" + str(self.numOfInfluencors)
 			counter = 0
 			for i in self.attractionCandidates:
 				counter += 1
 				#Calculate the Vectors from the Currents Nodes Position
 				abVector = (i[0] - self.position[0], i[1] - self.position[1], i[2] - self.position[2])
-				#print "Distance Vector  " + str(abVector) + "\n"
-				print "I am Node  " + str(self.position) + " Closest Point is" + str(i) + "\n"
+				##print "Distance Vector  " + str(abVector) + "\n"
+				#print "I am Node  " + str(self.position) + " Closest Point is" + str(i) + "\n"
 
 
-				self.averagePosition[0] += i[0]
-				self.averagePosition[1] += i[1]
-				self.averagePosition[2] += i[2]
+				self.averagePosition[0] += abVector[0] #i[0]
+				self.averagePosition[1] += abVector[1] #i[1]
+				self.averagePosition[2] += abVector[2] #i[2]
 
 			self.averagePosition[0] = self.averagePosition[0] / self.numOfInfluencors
 			self.averagePosition[1] = self.averagePosition[1] / self.numOfInfluencors
 			self.averagePosition[2] = self.averagePosition[2] / self.numOfInfluencors
-			#print "self av Pos" + str(self.averagePosition)
+			##print "self av Pos" + str(self.averagePosition)
 			return self.averagePosition
 
 	def createNextNode(self):
@@ -93,7 +95,7 @@ treeNodes.append(firstTreeNode)
 
 
 
-#print treeNodesHelper.index((0.0, 1.0, 0.0))
+##print treeNodesHelper.index((0.0, 1.0, 0.0))
 
 
 #Prepare list for kd-tree
@@ -110,17 +112,26 @@ counter = 0
 for i in range(0,numOfIterations):
 	#Populate Node Tree
 	del treeNodesHelper[:]
-	for j in treeNodes:
-		treeNodesHelper.append(j.getPosition())
 
-	print "##### overall debug ######"
-	for i in treeNodes: 
-		print i.position
-	print "\n\n"
-	print str(treeNodesHelper)
-	print "##### overall debug END ######" + "\n"
+	for j in treeNodes:
+		treeNodesHelper.append((j.getPosition()[0],j.getPosition()[1],j.getPosition()[2]) )
+
+	#print "##### overall debug ######"
+	#for i in treeNodes: 
+		#print i.position
+	#print "\n\n"
+	#print str(treeNodesHelper)
+	#print "##### overall debug END ######" + "\n"	
 	#Create Tree for Nodes
-	NodesTree = KDTree.construct_from_data(treeNodesHelper)
+	#
+	trashTreeHelper = copy.deepcopy(treeNodesHelper)
+	NodesTree = KDTree.construct_from_data(trashTreeHelper) #CONSTRUCT FROM DATA juggles the order!
+	#print "##### overall debug AFTER TREE ######"
+	#for i in treeNodes: 
+		#print i.position
+	#print "\n\n"
+	#print str(treeNodesHelper)
+	#print "##### overall debug END ######" + "\n"	
 	helperList = list()
 	#Find Candidates
 	Candidates = set()
@@ -130,35 +141,37 @@ for i in range(0,numOfIterations):
 			Candidates.add(oneNeighbour)
 
 	#Associate Canddiates with Nodes
-	print ("==== Associcated Nodes ===== \n")
+	#print ("==== Associcated Nodes ===== \n")
 	for i in Candidates:
 		nearestNode = NodesTree.query(query_point=(i), t = 1)
 		currentNodeIndex = treeNodesHelper.index(nearestNode[0])
-		print "Candidate: " + str((i[0], i[1], i[2])) + " ASSOC WITH*: " + str(nearestNode) +" " + str(currentNodeIndex) + "Index*:" #+ str(treeNodes[currentNodeIndex].position)
+		#print "Candidate: " + str((i[0], i[1], i[2])) + " ASSOC WITH*: " + str(nearestNode[0]) +"\n " +  "\nIndex*:" +str(currentNodeIndex) + " "+ str(treeNodes[currentNodeIndex].position)
 		treeNodes[currentNodeIndex].attractionCandidates.append(i)
-		print "current Attr Canddiates: " + str(treeNodes[currentNodeIndex].attractionCandidates) + "\n"
+		#print "current Attraction Canddiates: " + str(treeNodes[currentNodeIndex].attractionCandidates) + "\n"
 
 
-	print "== nodes processing ============================ " + str(counter) +"\n"
+
+	#print "== nodes processing ============================ " + str(counter) +"\n"
 	nodesCounter = 0
 	numNodes = len(treeNodes)
+	#print "length of nodes: " + str(numNodes)
 	for i in treeNodes:
 		if nodesCounter <= numNodes: #prevent overcounting
-			nodesCounter += 1
+			#nodesCounter += 1
 			if i.calculateAverageDir() != -1: #maybe do it on the fly?
-				print "====== next Node " + str(counter) +" ===== \n"
-				print str(i.position)
+				#print "====== next Node " + str(i.getPosition()) +" ===== \n"
+				#print str(i.position)
 
-				#print "function next noden" + str(i.createNextNode())
+				##print "function next noden" + str(i.createNextNode())
 				nextNodesPosition = i.createNextNode()
-				#print "Previous Tree-Nodes Position: " + str(i.getPosition())
+				##print "Previous Tree-Nodes Position: " + str(i.getPosition())
 				nextAveragePos = hou.Vector3(nextNodesPosition[0] +i.getPosition()[0] , nextNodesPosition[1] +i.getPosition()[1], nextNodesPosition[2] +i.getPosition()[2])
 				nextNode = treeNode(nextAveragePos)
 				treeNodes.append(nextNode)
 				
 				i.resetAll()
 			else:
-				print "this node has no candidates"
+				print "this node has no candidates " + str(i.getPosition())
 
 		nodesCounter += 1
 
@@ -167,13 +180,13 @@ for i in range(0,numOfIterations):
 
 
 #Create a visual feedback (debug)
-print "Nodes Total: " + str(len(treeNodes))
+#print "Nodes Total: " + str(len(treeNodes))
 for i in treeNodes:
 	tmp = geo.createPoint()
 	tmp.setAttribValue("P", i.position)
 	tmp.setAttribValue("Cd", hou.Vector3(0.0, 1.0, 0.0))
 
 
-print("===== END =====")
+#print("===== END =====")
 # Add code to modify contents of geo.
 # Use drop down menu to select examples.
